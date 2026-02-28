1172d5c18fb67019077e9a0a9a92b1ea81eec338
Frank Cort <frank@apptoto.com>
2026-02-28 11:20:26 -0800

    add pagination to sessions

--- app/controllers/console_agent/sessions_controller.rb
+++ app/controllers/console_agent/sessions_controller.rb
@@ -1,7 +1,12 @@
 module ConsoleAgent
   class SessionsController < ApplicationController
+    PER_PAGE = 50
+
     def index
-      @sessions = Session.recent.limit(100)
+      @page = [params[:page].to_i, 1].max
+      @total = Session.count
+      @total_pages = (@total / PER_PAGE.to_f).ceil
+      @sessions = Session.recent.offset((@page - 1) * PER_PAGE).limit(PER_PAGE)
     end
 
     def show
